7451d8c253cb196e4ecafbcdafe3e8614b2ea717
Sporego <contact@filbert.io>
2020-12-13 18:43:56 -0500

    initial chakra-ui wrapper w/ customTheme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+// Chakra-UI Wrapper => ChakraProvider
+import { ChakraProvider } from "@chakra-ui/react"
+// Import Chakra customizations as customTheme
+import customTheme from `../theme/theme.js`
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraProvider theme={customTheme} resetCSS="true">
+  <Component {...pageProps} />
+  </ChakraProvider>)
 }
 
 export default MyApp
