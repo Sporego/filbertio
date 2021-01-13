00067ed233e42c224faf78884d19ee80145e10d2
Sporego <contact@filbert.io>
2021-01-13 13:48:24 -0500

    Add Prettier preferences

--- theme/styles.js
+++ theme/styles.js
@@ -2,18 +2,10 @@ const styles = {
     
         styles: {
           global: {
-            // styles for the `body`
             body: {
               bg: "white",
               color: "black",
             },
-            // styles for the `a`
-            // a: {
-            //   color: "teal.500",
-            //   _hover: {
-            //     textDecoration: "underline",
-            //   },
-            // },
           },
         },
       
