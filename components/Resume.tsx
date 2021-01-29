69b6e5982c7e3fee40837ad23a3fe5ba66f6e23c
Sporego <contact@filbert.io>
2021-01-29 00:55:06 -0500

    PDFJS Resume Feature Completed

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
 	return (
 		<iframe
 			id='pdfjs'
-			src='./lib/pdfjs/web/viewer.html?file=../../../public/Filbert_Shi_Resume.pdf'
+			src='./lib/pdfjs/web/viewer.html?file=Filbert_Shi_Resume.pdf'
 			width='100%'
 		></iframe>
 	);
