76c6e4f648bc2bf7ed3bf48a44d173f05d2ef430
Sporego <contact@filbert.io>
2021-01-28 23:52:38 -0500

    resume initial test

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
 	return (
 		<iframe
 			id='special'
-			src='./lib/web/viewer.html?file=../../Filbert_Shi_Resume.pdf'
+			src='./lib/pdfjs/viewer.html?file=../../Filbert_Shi_Resume.pdf'
 			width='100%'
 		></iframe>
 	);
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,13 +1,21 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import Navbar from '../components/Navbar';
 
 export default function Home() {
+	const Resume = dynamic(
+		() => {
+			return import('../components/Resume');
+		},
+		{ ssr: false }
+	);
 	return (
 		<div>
 			<Head>
 				<title>Filbert Shi</title>
 			</Head>
 			<Navbar />
+			<Resume />
 		</div>
 	);
 }
