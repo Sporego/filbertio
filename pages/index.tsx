891c80aba8dbb997d5a0e5dceaad37afb770d7d3
Sporego <contact@filbert.io>
2021-02-07 23:19:28 -0500

    npx prettier --write .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
-import Resume from '../components/Resume'
+import Resume from '../components/Resume';
 
 export default function Home() {
 	return (
@@ -9,7 +9,7 @@ export default function Home() {
 				<title>Filbert Shi</title>
 			</Head>
 			<Navbar />
-			<Resume/>
+			<Resume />
 		</div>
 	);
 }
