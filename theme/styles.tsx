4028d8ca5c31a5e3d3325dddcfcaceea2725b3e4
Sporego <contact@filbert.io>
2021-04-29 22:57:01 -0400

    fixed styled.tsx

--- theme/styles.tsx
+++ theme/styles.tsx
@@ -1,22 +1,18 @@
 interface styles {
-	styles: {
-
-			body: {
-				bg: string;
-				color: string;
-			};
-
+	global: {
+		body: {
+			bg: string;
+			color: string;
+		};
 	};
 }
 
 const styles = {
-	styles: {
-
-			body: {
-				bg: 'white',
-				color: 'black',
-			},
-
+	global: {
+		body: {
+			bg: 'white',
+			color: 'black',
+		},
 	},
 };
 export default styles;
